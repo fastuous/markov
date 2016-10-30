c5831efe8f7a2cdd6c712e23c62f147b0c4dc977
Truman DeYoung <fastuous@gmail.com>
2016-10-29 18:54:07 -0600

    Started cleaning up

--- src/Main.hs
+++ src/Main.hs
@@ -22,7 +22,7 @@ data Token = Word | Char deriving Eq
 
 parsePair :: Parser T.Text -> Int -> Parser (Prefix, Suffix)
 parsePair p ord = lookAhead pair <* p where
-  pair = (,) <$> foldr T.append T.empty <$> replicateM ord p
+  pair = (,) <$> (foldr T.append T.empty <$> count ord p)
              <*> (M.singleton <$> p <*> pure 1)
 
 word :: Parser T.Text
@@ -51,26 +51,24 @@ markovGen = do
   (token, chain) <- ask
   let suffMap    = chain M.! prefix
       totalFreq  = M.foldr (+) 0 suffMap
-      suffMap'   = snd $ M.mapAccum (\a b -> (a + b, a + b)) 0 suffMap
       (rand, g') = randomR (1, totalFreq) g
+      suffMap'   = snd $ M.mapAccum (\a b -> (a + b, a + b)) 0 suffMap
       suffix     = fst . M.findMin . M.filter (>= rand) $ suffMap'
       prefix'    = shiftPrefix token prefix suffix
   liftIO $ T.putStr suffix
   put (g', prefix')
 
-evalMarkov :: MarkovGen -> (Token, Chain) -> (StdGen, Prefix) -> IO ()
-evalMarkov m r = evalStateT (runReaderT m r)
-
 generate :: Token -> Int -> Int -> Chain -> IO ()
 generate tok n ord chain = getStdGen >>= \g ->
   evalMarkov (replicateM_ n markovGen) (tok, chain) (g, start) where
     start = T.pack $ replicate ord '\NUL'
+    evalMarkov m r = evalStateT (runReaderT m r)
 
 main :: IO ()
 main = do
   args <- getArgs
   let ord   = read (head args) :: Int
-      n     = read (args!!1) :: Int
+      n     = read (args!!1)   :: Int
       token = if args!!2 == "word" then Word else Char
       files = mapM T.readFile (drop 3 args)
   inputs <- files
