8febf358a8abe76032337d2e253276d042efe1d2
Truman DeYoung <fastuous@gmail.com>
2016-10-29 18:22:43 -0600

    First working version

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,79 @@
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Attoparsec.Text as A
-import Data.Attoparsec.Combinator (lookAhead)
-import Data.Char (isSpace)
-import qualified Data.Map.Strict as M
-import qualified Data.Text as T
+import           Control.Applicative
+import           Control.Monad.Reader
+import           Control.Monad.State
+import           Data.Attoparsec.Combinator (lookAhead)
+import           Data.Attoparsec.Text       as A
+import           Data.Char                  (isSpace)
+import qualified Data.Map.Strict            as M
+import qualified Data.Text                  as T
+import qualified Data.Text.IO               as T
+import           System.Environment         (getArgs)
+import           System.Random
 
 type Chain  = M.Map Prefix Suffix
 type Prefix = T.Text
 type Suffix = M.Map T.Text Int
 
-test :: T.Text
-test = T.pack "The quick brown fox jumps over the lazy dog."
+type MarkovGen = ReaderT (Token, Chain) (StateT (StdGen, Prefix) IO) ()
 
-test' :: Int -> T.Text
-test' n = nonword `T.append` test `T.append` nonword where
-  nonword = T.pack $ replicate n '\NUL'
+data Token = Word | Char deriving Eq
 
-parsePair :: Int -> Parser (Prefix, Suffix)
-parsePair ord = lookAhead pair <* word where
-  pair = (,) <$> foldr T.append T.empty <$> replicateM ord word
-             <*> (M.singleton <$> word <*> pure 1)
+parsePair :: Parser T.Text -> Int -> Parser (Prefix, Suffix)
+parsePair p ord = lookAhead pair <* p where
+  pair = (,) <$> foldr T.append T.empty <$> replicateM ord p
+             <*> (M.singleton <$> p <*> pure 1)
 
 word :: Parser T.Text
 word = T.singleton <$> char '\NUL' <|>
        T.append <$> A.takeWhile1 isWordChar <*> A.takeWhile isSpace where
          isWordChar = (&&) <$> not . isSpace <*> (/= '\NUL')
 
-buildChain :: Int -> T.Text -> Chain
-buildChain ord input =
+buildChain :: Token -> Int -> T.Text -> Chain
+buildChain tok ord input =
   M.fromListWith (M.unionWith (+)) pairList where
-    pairList = either (const []) id $ parseOnly (many (parsePair ord)) input'
+    pairList = either (const []) id $ parseOnly (many (parsePair p ord)) input'
+    p = if tok == Word then word else T.singleton <$> anyChar
     input' = nonword `T.append` input `T.append` nonword
     nonword = T.pack $ replicate ord '\NUL'
 
+shiftPrefix :: Token -> Prefix -> T.Text -> Prefix
+shiftPrefix tok prefix suffix = prefix' `T.append` suffix where
+  prefix' = if tok == Char || T.head prefix == '\NUL'
+            then T.tail prefix
+            else T.dropWhile isSpace (T.dropWhile isWordChar prefix)
+  isWordChar = (&&) <$> not . isSpace <*> (/= '\NUL')
+
+markovGen :: MarkovGen
+markovGen = do
+  (g, prefix) <- get
+  (token, chain) <- ask
+  let suffMap    = chain M.! prefix
+      totalFreq  = M.foldr (+) 0 suffMap
+      suffMap'   = snd $ M.mapAccum (\a b -> (a + b, a + b)) 0 suffMap
+      (rand, g') = randomR (1, totalFreq) g
+      suffix     = fst . M.findMin . M.filter (>= rand) $ suffMap'
+      prefix'    = shiftPrefix token prefix suffix
+  liftIO $ T.putStr suffix
+  put (g', prefix')
+
+evalMarkov :: MarkovGen -> (Token, Chain) -> (StdGen, Prefix) -> IO ()
+evalMarkov m r = evalStateT (runReaderT m r)
+
+generate :: Token -> Int -> Int -> Chain -> IO ()
+generate tok n ord chain = getStdGen >>= \g ->
+  evalMarkov (replicateM_ n markovGen) (tok, chain) (g, start) where
+    start = T.pack $ replicate ord '\NUL'
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  args <- getArgs
+  let ord   = read (head args) :: Int
+      n     = read (args!!1) :: Int
+      token = if args!!2 == "word" then Word else Char
+      files = mapM T.readFile (drop 3 args)
+  inputs <- files
+  let chains = map (buildChain token ord) inputs
+      chain  = foldr (M.unionWith (M.unionWith (+))) M.empty chains
+  generate token n ord chain >> putChar '\n'
